d195943bfb5834a1608ca6f71c0a37747ba4ab39
Felipe Maffezzolli <felipe.maffezzolli@gmail.com>
2020-06-07 15:36:05 -0300

    Home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <h1>Hello World</h1>
-  );
-}
+import Home from './pages/Home'
 
-export default App;
+export default function App() {
+  return <Home />;
+}
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import './styles.css'
+
+import { FiLogIn } from 'react-icons/fi'
+
+import logo from '../../assets/logo.svg'
+
+const Home = () => {
+  return (
+    <div id="page-home">
+      <div className="content">
+        <header>
+          <img src={logo} alt="logo"/>
+        </header>
+
+        <main>
+          <h1>Seu marketplace de coleta de resíduos</h1>
+          <p>Ajudando pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+
+          <a href="/cadastro">
+            <span><FiLogIn /></span>
+            <strong>Cadastre um ponto de coleta</strong>
+          </a>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default Home
